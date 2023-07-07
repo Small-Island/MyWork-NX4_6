9ff3b07d8b6f2d0cb5e5f8187d84377953d1ade7
OJIMA-YUKIYA <ojima.yukiya@gmail.com>
2023-07-07 19:19:52 +0900

    update

--- twincam-motor-gui.sh
+++ twincam-motor-gui.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-gnome-terminal -- bash -c "cd /home/tristar/MyWork-NX4_6/; /home/tristar/MyWork-NX4_6/ojima_sentFromJetson.py"
+source /home/tristar/.bashrc
+systemctl --user stop twincam-motor.service
+gnome-terminal -- bash -c "echo twincam-motor.sh; source /home/tristar/.bashrc; cd /home/tristar/MyWork-NX4_6/; /home/tristar/MyWork-NX4_6/twincam-i2c.py"
