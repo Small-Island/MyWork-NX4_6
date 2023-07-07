051863baa9c82fe4164acd6a9dea8d2167e544b0
Small-Island <ojima.yukiya@gmail.com>
2023-07-07 18:55:29 +0900

    update

--- localsoraleftgui.sh
+++ localsoraleftgui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/tristar/.bashrc
-systemctl --user stop theta-momo.service
+systemctl --user stop twincam-theta.service
 gnome-terminal -- bash -c "echo localsoraleftgui.sh; source /home/tristar/.bashrc; cd /home/tristar/MyWork-NX4_6/; ./localsoraleft.py"
--- localsorarightgui.sh
+++ localsorarightgui.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/tristar/.bashrc
-systemctl --user stop theta-momo.service
+systemctl --user stop twincam-theta.service
 gnome-terminal -- bash -c "echo localsorarightgui.sh; source /home/tristar/.bashrc; cd /home/tristar/MyWork-NX4_6/; ./localsoraright.py"
