dbe84dac400ade860ca992dbe5f50888b73032c6
Small-Island <ojima.yukiya@gmail.com>
2023-07-07 18:34:47 +0900

    update

---
+++ twincam-motor.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sleep 4s
+cd /home/tristar/MyWork-NX4_6
+./local_webrtc_momo.run
+sleep 1s;
+./twincam-i2c.py
--- twincam-theta.sh
+++ twincam-theta.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sleep 4s
-cd /home/tristar/MyWork-NX4_6
-./local_webrtc_momo.run
-sleep 1s;
-./twincam-i2c.py &
-sleep 1s
+sleep 10s
 /home/tristar/MyWork-NX4_6/wakeup_theta.py
 sleep 1s
 /home/tristar/MyWork-NX4_6/wakeup_theta.py
